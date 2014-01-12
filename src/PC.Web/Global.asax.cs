1103258ea4c784e1a643c5e376448ecb162194ee
neutmute <neutemute.email>
2014-01-12 11:20:48 +1100

    clean up

--- src/PC.Web/Global.asax.cs
+++ src/PC.Web/Global.asax.cs
@@ -27,8 +27,6 @@ namespace PC.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            BootstrapSupport.BootstrapBundleConfig.RegisterBundles(System.Web.Optimization.BundleTable.Bundles);
-            //BootstrapMvcSample.ExampleLayoutsRouteConfig.RegisterRoutes(RouteTable.Routes);
         }
     }
 }
