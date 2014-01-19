6ca3d2f5fc89124e04d78042460fdd688e3f0844
neutmute <neutmute@neutmute.nm>
2014-01-19 20:34:38 +1100

    .

--- src/RPi.Nancy/Program.cs
+++ src/RPi.Nancy/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.AspNet.SignalR;
 using Microsoft.Owin.Hosting;
 using Nancy.Hosting.Self;
 using Common.Logging;
@@ -47,7 +46,7 @@ namespace RPi.NancyHost
     {
         public void Configuration(IAppBuilder app)
         {
-            app.MapSignalR();
+          //  app.MapSignalR();
             app.UseNancy();
         }
     }
